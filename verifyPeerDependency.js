62b90174275cae9a5f0e933a33d305e2def66919
Alasdair Hurst <omega@omegahurst.co.uk>
2017-11-14 22:55:02 +0000

    add version check

--- verifyPeerDependency.js
+++ verifyPeerDependency.js
@@ -1,5 +1,7 @@
 var package = require('./package.json');
 var semver = require('semver');
+var path = require('path');
+var findRoot = require('find-root');
 
 function error(err, shouldThrow) {
 	if (shouldThrow) {
@@ -17,6 +19,7 @@ function error(err, shouldThrow) {
  * @param {boolean} shouldThrow - if true, the function will throw rather than exiting the process
  */
 module.exports = function verifyPeerDependency(dependency, shouldThrow) {
+	console.info('verify', dependency);
 	var version = package.optionalPeerDependencies[dependency];
 	if (!version) {
 		return error(package.name + ' does not specify ' + dependency + ' as an optionalPeerDependency.', shouldThrow);
@@ -28,6 +31,15 @@ module.exports = function verifyPeerDependency(dependency, shouldThrow) {
 		return error(package.name + ' requires a dependency of ' + dependency + '@' + version + ' but none was installed.', shouldThrow);
 	}
 	console.log(dependencyPath);
+	// find the closest package.json
+	var root = findRoot(dependencyPath);
 
+	if (root === dependencyPath) {
+		return error('Cannot verify that ' + dependency + '@' + version + ' satisfies specified version', shouldThrow);
+	}
+	var dependencyPackage = require(path.join(root, 'package.json'));
+	if (!semver.valid(dependencyPackage.version)) {
+		return error(package.name + ' requires a dependency of ' + dependency + '@' + version + ' but none was installed.', shouldThrow);
+	}
 	// check version
 }
